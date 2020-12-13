75545f8841c584a201220eb9e260639fc66141d9
Antonio Krušlin <kruslin.antonio@gmail.com>
2020-12-13 19:37:56 +0100

    Finished Task 4

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -1,29 +1,25 @@
-﻿using ContactManager.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using ContactManager.Models;
+using ContactManager.Services;
+
 namespace ContactManager.Controllers
 {
     public class ContactController : Controller
     {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
         public Contact[] Get()
         {
-            return new Contact[]
-    {
-        new Contact
-        {
-            Id = 1,
-            Name = "Glenn Block"
-        },
-        new Contact
-        {
-            Id = 2,
-            Name = "Dan Roth"
-        }
-    };
+            return contactRepository.GetAllContacts();
         }
     }
 }
